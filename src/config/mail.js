5daa5041d4089690e1b7a255f4d84e767bad70d2
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2020-02-18 15:43:07 -0300

    Configura nodemailer para enviar emails de cancelamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '7effdea9f586d8',
+    pass: '0c9d133d51e5ee',
+  },
+  default: {
+    from: 'Team GoBarber <noreply@gobarber.com>',
+  },
+};
